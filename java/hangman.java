04714f5e448eb5fd6fe73ef1286e00962c490d5f
pedro-fs-garcia <pedrofsgarcia.pro@gmail.com>
2024-07-04 19:07:53 -0300

    forca java

--- java/hangman.java
+++ java/hangman.java
@@ -4,7 +4,7 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.*;
 
-public class forca{
+public class hangman{
     public static void main(String[] args) {
         System.out.println("Vamos nos enforcar!");
         playRound();
