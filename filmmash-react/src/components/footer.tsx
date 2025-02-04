09b863519f8ce0983d73fc56d347f15552027285
Pedro Garcia <pedrofsgarcia.pro@gmail.com>
2025-02-03 23:55:43 -0300

    exclui redundancia

--- filmmash-react/src/components/footer.tsx
+++
@@ -1,9 +0,0 @@
-function Footer(){
-    return(
-        <footer className="w-full bg-gray-800 text-white py-8 px-16 items-center">
-            <p className="w-full text-center">&copy; {new Date().getFullYear()} FilmMash</p>
-        </footer>
-    );
-}
-
-export default Footer
