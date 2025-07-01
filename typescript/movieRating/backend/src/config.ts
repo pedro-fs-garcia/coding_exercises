0076e5fe406f2f7cf093e38b2b304d6a85cd19e5
pedro garcia <pedrofsgarcia.pro@gmail.com>
2025-07-01 13:49:21 -0300

    delete movierating

--- typescript/movieRating/backend/src/config.ts
+++
@@ -1,7 +0,0 @@
-import dotenv from 'dotenv';
-
-dotenv.config();
-
-export const frontendServer = process.env.FRONTEND_SERVER;
-export const backendServer = process.env.BACKEND_SERVER;
-export const jwtSecret = process.env.JWT_SECRET;
--- typescript/movieRating/frontend/src/routes/ProtectedRoute.tsx
+++
@@ -1,9 +0,0 @@
-import { Navigate, Outlet } from "react-router-dom";
-import User from "../models/User";
-
-const ProtectedRoute = () => {
-    const user:User|null = JSON.parse(sessionStorage.getItem('user') || "null");
-    return user ? <Outlet /> : <Navigate to="/login" />;
-  };
-  
-  export default ProtectedRoute;
